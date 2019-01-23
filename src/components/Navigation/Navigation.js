f73609a73861c6016a33ba2680bbb6e53c3c7202
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-23 17:48:09 +0000

    adding react-boostrap and the nav components with boostrap

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,1 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+ class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"> <a className="text-light mr-auto"><img src="./../../assets/images/projects/favicon.png" style={{width:'80px'}} alt="favicon am" /></a></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav  navbar>
+              <NavItem>
+                <NavLink href="/components/">ABOUT</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">CONTACT</NavLink>
+              </NavItem>
+              
+              <NavItem >
+                <NavLink href="/components/"> <a title="Spanish" class="nav-link text-white" id="spain" href="#"><img src="./../../assets/images/spain-flag-icon-32.png" alt="spain flag" /></a></NavLink>
+              </NavItem>
+              <NavItem className="ml-auto">
+                <NavLink href="/components/"><a title="English" class="nav-link text-white" id="england" href="#"><img src="./../../assets/images/projects/spain-flag-icon-64.png" alt="england flag" /></a></NavLink>
+              </NavItem>
+              </Nav>
+           
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+export default Navigation;
