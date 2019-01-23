6f58f3fbcee0f72384528eaaf881e0946cfa34f8
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-23 19:35:40 +0000

    adding more componets to layout

---
+++ src/components/Presentacion/Presentation.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Title from '../Title/Title';
+import SectionTitle from './../SectionTitle/';
+import './Contact.css';
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+const Presentation = () => (
+	<div>
+		<SectionTitle title="Presentation" />
+
+	</div>
+);
+
+export default Presentation;
---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+
+class SectionTitle extends Component {
+  render() {
+    return (
+        <h2 class="font mb-1 text-dark mt-5 mb-1" id="" data-aos="fade-right"></h2>
+    );
+  }
+}
+
+export default SectionTitle;
