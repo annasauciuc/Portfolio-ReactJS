3ff7f76b824a4e38d9fbcba318ac1b2c985e4af6
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-24 23:54:29 +0000

    adding the projects list in cards

--- src/components/HireCards/HireCards.js
+++ src/components/HireCards/HireCards.js
@@ -4,26 +4,9 @@ import HireCard from "./../HireCard/HireCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./HireCards.css";
 import { CardDeck } from "reactstrap";
+import projects from './../../projects'
 const HireCards = () => {
-  
-  const projects = [{
-    id: 1,
-    className: 'fas fa-fist-raised',
-    text: 'The ability to collaborate with clients and peers is crucial for web development. Respecting the others ideas and space also. Nothing is more rewarding when hard work pays off. Always trying to learn new things or reinforcing what i know.',
-    title: 'Hard Worker'
-},{
-    id: 2,
-    className: 'fas fa-heart',
-    text: 'Found what you like and let it kill you...well, sound so dramatic but i think the greatest work is done by passion, people that love what they do for a living.',
-    title: 'Passionate'
-},{
-    id: 3,
-    className: 'fas fa-fast-forward',
-    text: "Never stop being curious. I'm confident in my ability to learn something new and put it into production. ",
-    title: 'Fast Learner'
-}
 
-];
 console.log("projects :", projects);
   return (
     <CardDeck>
--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-export const projects = [{
+ const projects = [{
     id: 1,
     className: 'fas fa-fist-raised',
     text: 'The ability to collaborate with clients and peers is crucial for web development. Respecting the others ideas and space also. Nothing is more rewarding when hard work pays off. Always trying to learn new things or reinforcing what i know.',
@@ -31,3 +31,4 @@ export const projects = [{
 }
 
 ];
+export default projects;
