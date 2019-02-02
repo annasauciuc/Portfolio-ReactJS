e8f776089825525c9d790eeafe3fa216e2f4019e
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-02 12:11:06 +0000

    styling the notfoundpage to be responsive

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,35 +4,36 @@ import "./PageNotFound.css";
 const PageNotFound = () => {
   return (
     <React.Fragment>
-      <body>
-        <section id="not-found">
-          <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
-          <div class="circles">
+      <div className="body">
+      
+          
+          <div className="circles  projectsTitle">
             <p>
               404
               <br />
               <small>PAGE NOT FOUND</small>
             </p>
-            <span class="circle big" />
-            <span class="circle med" />
-            <span class="circle med" />
-            <span class="circle small" />
-            <span class="circle big" />
-          </div>
-          <div className="m-5">
-            <h1>
+            <span className="circle big" />
+            <span className="circle med" />
+            <span className="circle med" />
+            <span className="circle small" />
+            <span className="circle big" />
+            <div className="m-5">
+            <button className="btn-lg btn">
               {" "}
               <a
-                title="Go Back to Main Page"
+                title="Back to Main Page"
                 href="/"
                 rel="noopener noreferrer"
               >
-                <strong>GO BACK TO MAIN PAGE</strong>
+                <strong>BACK TO MAIN PAGE</strong>
               </a>
-            </h1>
+            </button>
           </div>
-        </section>
-      </body>
+          </div>
+         
+       
+      </div>
     </React.Fragment>
   );
 };
