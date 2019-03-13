596802b2c8be1f3d610809465a1a6c55762006b0
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-03-13 01:21:23 +0000

    Updates

---
+++ precache-manifest.8ff81121aa229f40474cfa50e568c641.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a7e9a3eae64890c43f269dafd0fa38",
+    "url": "/myPage-portofolio/static/media/quote.73a7e9a3.jpg"
+  },
+  {
+    "revision": "daddb93109bffb9d422d",
+    "url": "/myPage-portofolio/static/css/main.2efff323.chunk.css"
+  },
+  {
+    "revision": "e3e0e2885a0d9820c5665c72c2af2d88",
+    "url": "/myPage-portofolio/static/media/firstProject.e3e0e288.jpg"
+  },
+  {
+    "revision": "67379897f54cf0a7316b",
+    "url": "/myPage-portofolio/static/js/1.67379897.chunk.js"
+  },
+  {
+    "revision": "053d29a1f05088cb63514155ba5c5c6d",
+    "url": "/myPage-portofolio/static/media/floats.053d29a1.jpg"
+  },
+  {
+    "revision": "b189a909936d7673ea21",
+    "url": "/myPage-portofolio/static/js/2.b189a909.chunk.js"
+  },
+  {
+    "revision": "16b8acc269db120cdf68",
+    "url": "/myPage-portofolio/static/js/runtime~main.16b8acc2.js"
+  },
+  {
+    "revision": "df358844eadbff33c8e15eb4b0230525",
+    "url": "/myPage-portofolio/static/media/personalImg.df358844.jpg"
+  },
+  {
+    "revision": "daddb93109bffb9d422d",
+    "url": "/myPage-portofolio/static/js/main.daddb931.chunk.js"
+  },
+  {
+    "revision": "d547d68baca2a53c76518384319bc41b",
+    "url": "/myPage-portofolio/static/media/netflix.d547d68b.jpg"
+  },
+  {
+    "revision": "74c39285f60285438fddf11ac50e794d",
+    "url": "/myPage-portofolio/static/media/form.74c39285.jpg"
+  },
+  {
+    "revision": "471f0177f3a7d0901de6ddcda90946da",
+    "url": "/myPage-portofolio/static/media/sass.471f0177.jpg"
+  },
+  {
+    "revision": "4dc889242113e27254a9e533344a33b1",
+    "url": "/myPage-portofolio/static/media/panel.4dc88924.jpg"
+  },
+  {
+    "revision": "b189a909936d7673ea21",
+    "url": "/myPage-portofolio/static/css/2.9d5823c9.chunk.css"
+  },
+  {
+    "revision": "67379897f54cf0a7316b",
+    "url": "/myPage-portofolio/static/css/1.61e4dfc2.chunk.css"
+  },
+  {
+    "revision": "de1c02ab33b43e1b7ef69c8ed2d9bb4f",
+    "url": "/myPage-portofolio/index.html"
+  }
+];
